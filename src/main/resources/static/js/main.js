c9036e5ef23c94222506b0651bc0664bf99dc748
difly <kryshen.yaroslav@gmail.com>
2019-05-24 20:16:18 +0300

    Get data list from server to front vue js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,5 @@
+var messageApi = Vue.resource('/message{/id}');
+
 Vue.component('message-row', {
     props: ['message'],
     template: '<div><i>( {{message.id }} )</i> {{ message.text }}</div>'
@@ -5,17 +7,22 @@ Vue.component('message-row', {
 
 Vue.component('messages-list', {
     props: ['messages'],
-    template: '<div><message-row v-for="message in messages" :message="message" /></div>'
+    template: '<div>' +
+            '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+        '</div>',
+    created: function () {
+        messageApi.get().then(result =>
+            result.json().then( data =>
+               data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
 });
 
 var app = new Vue({
     el: '#app',
     template: '<messages-list :messages="messages" />',
     data: {
-        messages: [
-            {id: '0', text: 'Изучить JavaScript'},
-            {id: '1', text: 'Изучить Vue'},
-            {id: '2', text: 'Создать что-нибудь классное'}
-        ]
+        messages: []
     }
 });
