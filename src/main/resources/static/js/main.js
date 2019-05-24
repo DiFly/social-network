30cca55d2fc9af3ec3ecc3ec6210e0d04e1024ed
difly <kryshen.yaroslav@gmail.com>
2019-05-24 16:39:18 +0300

    Add vue list for render on page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,21 @@
-Vue.component('todo-item', {
-    template: '<li>Это одна задача в списке</li>'
-})
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div><i>( {{message.id }} )</i> {{ message.text }}</div>'
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div><message-row v-for="message in messages" :message="message" /></div>'
+});
 
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages" />',
     data: {
-        message: 'Hello Vue!'
+        messages: [
+            {id: '0', text: 'Изучить JavaScript'},
+            {id: '1', text: 'Изучить Vue'},
+            {id: '2', text: 'Создать что-нибудь классное'}
+        ]
     }
 });
