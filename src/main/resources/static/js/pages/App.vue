30c0b86ba6cf1a5afb1cc8d64dcb659ab58a758b
difly <kryshen.yaroslav@gmail.com>
2019-06-20 00:09:47 +0300

    Add vue component to application

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,8 +1,29 @@
 <template>
+    <div>
+        <div v-if="!profile">Must to login from
+            <a href="/login">GOOGLE</a>
+        </div>
+        <div v-else>
+            <div>{{profile.name}}&nbsp;<a href="/logout">Logout</a></div>
+            <messages-list :messages="messages" />
+        </div>
+    </div>
 
 </template>
 
 <script>
+    import MessagesList from 'components/messages/MessageList.vue'
+    export default {
+        components:{
+            MessagesList
+        },
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile
+            }
+        }
+    }
 
 </script>
 
