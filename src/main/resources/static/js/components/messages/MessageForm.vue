ce1769d2783b4f67ed8ea24fc49a87c9be0f2d84
difly <kryshen.yaroslav@gmail.com>
2019-07-02 00:25:29 +0300

    Complete added sockjs and stompjs

--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -6,14 +6,8 @@
 </template>
 
 <script>
-    function getIndex(list, id) {
-        for (let i = 0; i<list.length; i++) {
-            if (list[i].id === id) {
-                return i
-            }
-        }
-        return -1
-    }
+    import { sendMessage } from "util/ws"
+
     export default {
         props: ['messages', 'messageAttr'],
         data() {
@@ -31,7 +25,12 @@
 
         methods: {
             save() {
-                const message = { text: this.text}
+                sendMessage({id: this.id, text: this.text})
+
+                this.text = ''
+                this.id = ''
+
+                /*const message = { text: this.text}
 
                 if (this.id) {
                     this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
@@ -49,7 +48,7 @@
                             this.text = ''
                         })
                     )
-                }
+                }*/
             }
         }
     }
