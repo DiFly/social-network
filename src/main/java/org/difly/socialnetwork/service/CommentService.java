7118c1f589132e0e3d4c63be1b76d33633919f54
difly <kryshen.yaroslav@gmail.com>
2019-08-12 23:26:49 +0300

    Add avatar to message and comment

--- src/main/java/org/difly/socialnetwork/service/CommentService.java
+++ src/main/java/org/difly/socialnetwork/service/CommentService.java
@@ -2,23 +2,33 @@ package org.difly.socialnetwork.service;
 
 import org.difly.socialnetwork.domain.Comment;
 import org.difly.socialnetwork.domain.User;
+import org.difly.socialnetwork.domain.Views;
+import org.difly.socialnetwork.dto.EventType;
+import org.difly.socialnetwork.dto.ObjectType;
 import org.difly.socialnetwork.repository.CommentRepository;
+import org.difly.socialnetwork.util.WsSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.function.BiConsumer;
 
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final BiConsumer<EventType, Comment> wsSender;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository, WsSender wsSender) {
         this.commentRepository = commentRepository;
+        this.wsSender = wsSender.getSender(ObjectType.COMMENT, Views.FullComment.class);
     }
 
     public Comment create(Comment comment, User user){
         comment.setAuthor(user);
-        commentRepository.save(comment);
+        Comment commentFromDb = commentRepository.save(comment);
 
-        return comment;
+        wsSender.accept(EventType.CREATE, comment);
+
+        return commentFromDb;
     }
 }
