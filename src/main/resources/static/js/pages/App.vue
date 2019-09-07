d8c9137f288719b7d90bd43fd91eb7f6fac195ad
difly <kryshen.yaroslav@gmail.com>
2019-09-07 22:03:54 +0300

    Add method to view profile of state user

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -14,7 +14,7 @@
 
             <v-btn flat
                    v-if="profile"
-                   :disabled="$route.path === '/profile'"
+                   :disabled="$route.path === '/user'"
                     @click="showProfile">
                 {{profile.name}}
             </v-btn>
@@ -47,7 +47,7 @@
                 this.$router.push('/')
             },
             showProfile(){
-                this.$router.push('/profile')
+                this.$router.push('/user')
             }
         },
         created() {
